8a00bbbbaca1287c52d67849a4fa4b5a055e81d7
Bhavya Joshi <bhavya.joshi1299@gmail.com>
2020-09-06 17:09:04 +0530

    Update Q1.cpp

--- C++ Assignment/Q1.cpp
+++ C++ Assignment/Q1.cpp
@@ -8,10 +8,10 @@
 #define ALPHABETS 26
 using namespace std;
 
-struct TrieNode  
+struct TrieNode  					//To store the nodes
 { 
     struct TrieNode* children[ALPHABETS]; 
-	int freq;  
+    int freq;  						//Frequency
     bool isLeaf; 
 }; 
 
@@ -21,7 +21,7 @@ struct TrieNode* getNode()
     for (int i = 0; i < ALPHABETS; i++) 
         pNode->children[i] = NULL;   
     pNode->isLeaf = false; 
-	pNode->freq=0;  
+    pNode->freq=0;  
     return pNode; 
 }; 
 
@@ -30,19 +30,15 @@ void insert(struct TrieNode* root, string key,int freq)
     struct TrieNode* R = root;   
     for (int i = 0; i < key.length(); i++)  
     { 
-        int index = key[i] - 'a';   
+        int index = key[i] - 'a'; 			//To store each character of given key in the tree  
         if (R->children[index] == NULL) 
             R->children[index] = getNode();   
         R = R->children[index]; 
     }   
-    R->isLeaf = true; 
+    R->isLeaf = true;					//Leaf nodes true 
     R->freq=freq;
 } 
 
-bool isLeafNode(struct TrieNode* root) 
-{ 
-    return root->isLeaf != false; 
-} 
 
 void search(struct TrieNode *root, string key) 
 { 
@@ -50,7 +46,7 @@ void search(struct TrieNode *root, string key)
     int f=0;  
     for (int i = 0; i < key.length(); i++) 
     { 
-        int index = key[i] - 'a'; 
+        int index = key[i] - 'a'; 				//Searches for the key by traversing th tree
         if (!R->children[index])
 		{
 			cout<<"NO";
@@ -65,7 +61,7 @@ void search(struct TrieNode *root, string key)
     if(f==0 && R != NULL && R->isLeaf)
     {
     	cout<<"YES "<<R->freq;
-	}
+    }
 	else
 	cout<<"NO";
 } 
@@ -75,25 +71,25 @@ int main(int argc, char *argv[])
 	
     struct TrieNode *root = getNode();   
 	
-	std::ifstream ifile(argv[1]);
+    std::ifstream ifile(argv[1] + std::string(".csv"));			//Pass filename
     std::string line; 
     while (std::getline(ifile, line)) // read the current line
     {
         std::istringstream iss{line}; 
         std::vector<std::string> tokens; 
         std::string token; 
-        while (std::getline(iss, token, ','))
+        while (std::getline(iss, token, ','))			//vector of each row
         {
             tokens.push_back(token); 
         }
 
        
         std::cout << std::endl;
-        std::string key = tokens[0]; 
-        int f = std::stoi(tokens[1]); 
+        std::string key = tokens[0]; 				//extract key from vector
+        int f = std::stoi(tokens[1]); 				//extract integer from vector
         for (int i = 0, len = key.size(); i < len; i++) 
           { 
-              if (ispunct(key[i])) 
+              if (ispunct(key[i])) 				//Remove apostrophes to store just alphabets in tree
             { 
                 key.erase(i--, 1); 
                 len = key.size(); 
@@ -105,12 +101,12 @@ int main(int argc, char *argv[])
     }
     
    int ch=0;
-   while(1)
+   while(1)								//Search until required
    { 
     string key;
     cout<<"\nEnter word to search\n";
     cin>>key;
-    for (int i = 0, len = key.size(); i < len; i++) 
+    for (int i = 0, len = key.size(); i < len; i++) 			//Remove apostrophes to search
           { 
               if (ispunct(key[i])) 
             { 
